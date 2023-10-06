4e85f2f17cdaf2008be6c89ac4b61256ac072007
Abdelrahman Hamada <abdelrahmanhamada65@gmail.com>
2023-10-07 02:15:23 +0300

    debug

--- adminpanel/settings.py
+++ adminpanel/settings.py
@@ -28,7 +28,7 @@ dotenv.load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
